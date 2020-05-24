3f5d1ef18c2fd956a3d361c61ef2c5295b5c199f
Cherry2710 <65523407+Cherry2710@users.noreply.github.com>
2020-05-24 14:58:41 +0700

    Update check_armstrong.py

--- exercise/check_armstrong.py
+++ exercise/check_armstrong.py
@@ -1,8 +1,9 @@
 def check_armstrong (number):
   sum = 0
-  
+  m = number
   n = len(str(number))
   
-sum += (num%10)**n 
-sum+= number%10**n
-number//= 10
+  sum += (num%10)**n 
+  sum+= number%10**n
+  number//= 10
+  return sum == m
